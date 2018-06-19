b959bdc8255a470ee0c191e47a4e44f269d4692d
Dat Nguyen <itsdatnguyen@gmail.com>
2018-06-19 11:58:22 -0400

    Added finishing touches to SEO and images

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,6 +68,11 @@ module.exports = {
         property: 'og:site_name',
         content: "Dat Nguyen's Repository"
       },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: './meta-image.png'
+      },
       { 
         hid: 'twitter:card', // twitter card
         name: 'twitter:card',
@@ -92,14 +97,53 @@ module.exports = {
         hid: 'twitter:creator',
         name: 'twitter:creator',
         content: '@itsdatnguyen'
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: './meta-image.png'
+      },
+      {
+        name: 'msapplication-TileColor',
+        content: '#da532c'
+      }, 
+      {
+        name: 'theme-color',
+        content: '#ffffff'
       }
     ],
     link: [
       {
+        rel: 'apple-touch-icon',
+        sizes: "180x180",
+        href: "/apple-touch-icon.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png"
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png"
+      },
+      {
         rel: "icon",
         type: "image/x-icon",
         href: "/favicon.ico"
-      }
+      },
+      {
+        rel: "manifest",
+        href: "/site.webmanifest"
+      },
+      {
+        rel: "mask-icon",
+        href: "/safari-pinned-tab.svg",
+        color: "#5bbad5"
+      },
     ]
   },
   /*
